d9185a11f9637a49f08067e70390c82ff727d47a
Rikard Engström <rikardengstrom89@gmail.com>
2024-12-10 17:21:31 +0100

    Added some logic when guessing a number.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const checkButton = document.querySelector(".check");
+
+checkButton.addEventListener("click", () => {
+  const guessNumber = Number(document.querySelector(".guess").value);
+
+  if (!guessNumber) {
+    document.querySelector(".message").textContent = "Please enter a number.";
+  } else {
+    document.querySelector(".message").textContent = guessNumber;
+  }
+});
