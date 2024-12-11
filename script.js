e647207cb84fa8968a1c4c88d919f8f05d551584
Rikard Engström <rikardengstrom89@gmail.com>
2024-12-11 14:31:54 +0100

    Added some stuff!

--- script.js
+++ script.js
@@ -10,17 +10,36 @@ checkButton.addEventListener("click", () => {
   const guessNumber = Number(document.querySelector(".guess").value);
   const message = document.querySelector(".message");
   const scoreText = document.querySelector(".score");
+  const bodyHtml = document.querySelector("body");
+  const secretNumberText = document.querySelector(".number");
+
+  //When there is no input
   if (!guessNumber) {
     message.textContent = "🚩 Please enter a number.";
+    //When the player wins
   } else if (guessNumber === secretNumber) {
     message.textContent = "🎉 You guessed the correct number!";
+    bodyHtml.style.backgroundColor = "#60b347";
+    secretNumberText.style.width = "30rem";
+    //When the player guess to high
   } else if (guessNumber > secretNumber) {
-    message.textContent = "📈 Your number was to high.";
-    score--;
-    scoreText.textContent = score;
+    if (score > 1) {
+      message.textContent = "📈 Your number was to high.";
+      score--;
+      scoreText.textContent = score;
+    } else {
+      message.textContent = "🤯 You lost the game!";
+      scoreText.textContent = 0;
+    }
+    //when the player guess to low
   } else if (guessNumber < secretNumber) {
-    message.textContent = "📉 Your number was to low.";
-    score--;
-    scoreText.textContent = score;
+    if (score > 1) {
+      message.textContent = "📉 Your number was to low.";
+      score--;
+      scoreText.textContent = score;
+    } else {
+      message.textContent = "🤯 You lost the game!";
+      scoreText.textContent = 0;
+    }
   }
 });
