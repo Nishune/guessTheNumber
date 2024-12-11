bc0f01ae8d6975d5eca191f458397b6266d3176d
Rikard Engström <rikardengstrom89@gmail.com>
2024-12-11 14:03:23 +0100

    Added some more logic.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 const checkButton = document.querySelector(".check");
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
 
 checkButton.addEventListener("click", () => {
   const guessNumber = Number(document.querySelector(".guess").value);
-
+  const message = document.querySelector(".message");
+  const scoreText = document.querySelector(".score");
   if (!guessNumber) {
-    document.querySelector(".message").textContent = "Please enter a number.";
-  } else {
-    document.querySelector(".message").textContent = guessNumber;
+    message.textContent = "🚩 Please enter a number.";
+  } else if (guessNumber === secretNumber) {
+    message.textContent = "🎉 You guessed the correct number!";
+  } else if (guessNumber > secretNumber) {
+    message.textContent = "📈 Your number was to high.";
+    score--;
+    scoreText.textContent = score;
+  } else if (guessNumber < secretNumber) {
+    message.textContent = "📉 Your number was to low.";
+    score--;
+    scoreText.textContent = score;
   }
 });
